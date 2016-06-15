1a01bb17a1b9013079d493d6cf94d9330c2977f4
Kevin Epstein <kevin@epstein.co.za>
2016-06-15 07:55:35 -0700

    minor change to have powershell show windows services after installing CodeDeploy

--- install_codedeploy.ps1
+++ install_codedeploy.ps1
@@ -3,3 +3,6 @@
 New-Item -Path c:\temp -ItemType "directory" -Force
 Read-S3Object -BucketName aws-codedeploy-us-east-1/latest -Key codedeploy-agent.msi -File c:\temp\codedeploy-agent.msi
 Start-Process -Wait -FilePath c:\temp\codedeploy-agent.msi -WindowStyle Hidden
+
+Get-Service
+Get-Service | Where-Object {$_.status -eq "running"}
