8a610fa1fcae845998e34aca85d578fdcc4857ec
Kevin Epstein <kepstein@corpinfo.com>
2016-06-15 23:17:28 -0700

    minor updates to codedeploy install script

--- install_codedeploy.ps1
+++ install_codedeploy.ps1
@@ -4,5 +4,5 @@ New-Item -Path c:\temp -ItemType "directory" -Force
 Read-S3Object -BucketName aws-codedeploy-us-east-1/latest -Key codedeploy-agent.msi -File c:\temp\codedeploy-agent.msi
 Start-Process -Wait -FilePath c:\temp\codedeploy-agent.msi -WindowStyle Hidden
 
-Get-Service | Where-Object {$_.DisplayName -eq "CodeDeploy"}
+Get-Service | Where-Object {$_.displayname -eq "CodeDeploy"}
 Get-Service | Where-Object {$_.status -eq "running"}
